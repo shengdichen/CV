023b410974df2f3f7ee34da629c6149be8325d87
shc <sh@c>
2024-07-17 18:32:02 -0700

    feat(util): push more artifacts

--- utils/push_to_remote.sh
+++ utils/push_to_remote.sh
@@ -4,15 +4,32 @@ __by_scp() {
     scp "${1}" "${2}"
 }
 
-__push() {
+__by_scp_xyz() {
     __by_scp \
-        "../src/main.pdf" \
-        "ssh_xyz:domains/shengdichen.xyz/public_html/CV.pdf"
+        "${1}" \
+        "ssh_xyz:domains/shengdichen.xyz/public_html/${2}"
 }
 
-main() {
-    __push
-    unset -f __by_scp __push
+__push() {
+    local _pdf_only""
+    if [ "${1}" = "--pdf-only" ]; then
+        _pdf_only="yes"
+    fi
+
+    __by_scp_xyz "../src/main.pdf" "CV.pdf"
+
+    find "../ms" -mindepth 1 -type f | while read -r _f; do
+        printf "> [%s]\n" "${_f}"
+
+        if [ "${_pdf_only}" ]; then
+            if printf "%s" "${_f}" | grep -q "\.pdf$"; then
+                __by_scp_xyz "${_f}" "$(basename "${_f}")"
+            fi
+        else
+            __by_scp_xyz "${_f}" "$(basename "${_f}")"
+        fi
+
+    done
 }
-main
-unset -f main
+
+__push "${@}"
